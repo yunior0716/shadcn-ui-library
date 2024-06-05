e8b3b80f3f98fdc4e073d25535f1c6d6321466d3
Delio Yunior <yunior0716@gmail.com>
2024-06-04 23:06:30 -0400

    updated home and footer

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,7 +1,18 @@
 export default function Page() {
   return (
     <div>
-      <h1>Shadcn/ui Library</h1>
+      <h1 className="text-xl pb-4">Shadcn/ui Library</h1>
+      <p>
+        This is a library of reusable components made with{' '}
+        <a
+          href="https://ui.shadcn.com/"
+          target="_blank"
+          className="text-blue-800 text-lg dark:text-slate-500"
+        >
+          shadcn/ui
+        </a>
+        .
+      </p>
     </div>
   );
 }
