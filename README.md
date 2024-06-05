0ffa3eefeffdfc649bef2bec775728d69086de33
Delio Yunior <yunior0716@gmail.com>
2024-06-04 22:20:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+This is a Shadcn ui components library made with Tailwind CSS and Next.js.
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from 'next';
+import { Inter as FontSans } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/ui/theme-provider';
+
+import { Toaster as SonnerToaster } from '@/components/ui/sonner';
+import { Toaster } from '@/components/ui/toaster';
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+export const metadata: Metadata = {
+  title: 'Shadcn - Delio Díaz',
+  description:
+    'Shadcn ui components and layout made with Tailwind CSS and Next.js',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+
+        <SonnerToaster richColors />
+        <Toaster />
+      </body>
+    </html>
+  );
+}
